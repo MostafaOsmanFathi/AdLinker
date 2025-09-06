a87b0d54c7a2ac8c69ed49ffe4a91f279efa2f26
mostafa <mostafa.osman.fathi@gmail.com>
2025-09-07 01:32:02 +0300

    refactoring DB url to be in config/env

--- config/env.js
+++ config/env.js
@@ -1,3 +1,4 @@
 const JWT_SECRET = "MOSTAFAOSMAN";
+const DB_URL = "mongodb://localhost:27017/BackendProject"
 
-module.exports = {JWT_SECRET};
+module.exports = {JWT_SECRET, DB_URL};
--- database/monogodbConnection.js
+++ database/monogodbConnection.js
@@ -1,7 +1,8 @@
 const mongoDB = require("mongoose");
+const {DB_URL} = require("../config/env")
 
 const connection = mongoDB
-    .connect("mongodb://localhost:27017/BackendProject")
+    .connect(DB_URL)
     .then(() => console.log("MongoDB On BackendProject Connected successfully"))
     .catch((err) => console.error(err));
 
