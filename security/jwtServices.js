25754f58f06c6011a4d7d81868998b9a1970ae80
mostafa <mostafa.osman.fathi@gmail.com>
2025-09-14 02:56:47 +0300

    refactor jwtServices from Set to node-Cash

--- security/jwtServices.js
+++ security/jwtServices.js
@@ -1,28 +1,25 @@
-const tokenBlackList = new Set()
+const NodeCache = require("node-cache");
+const blackListCashe = new NodeCache();
 //TODO Refactor from set to redis
 
 const checkTokenInBlackList = (token) => {
-    return !tokenBlackList.has(token);
+    return !blackListCashe.has(token);
 }
 
-const getTokenExpiryInMillisSeconds = (tokenExpiry) => {
+const getTokenExpiryInSeconds = (tokenExpiry) => {
     const tokenUnite = tokenExpiry[tokenExpiry.length - 1];
     tokenExpiry = Number(tokenExpiry.slice(0, -1));
 
 
-    if (tokenUnite === 'd')      return tokenExpiry * 24 * 60 * 60 * 1000;
-    else if (tokenUnite === 'h') return tokenExpiry * 60 * 60 * 1000;
-    else if (tokenUnite === 'm') return tokenExpiry * 60 * 1000;
-    else if (tokenUnite === 's') return tokenExpiry * 1000;
+    if (tokenUnite === 'd') return tokenExpiry * 24 * 60 * 60;
+    else if (tokenUnite === 'h') return tokenExpiry * 60 * 60;
+    else if (tokenUnite === 'm') return tokenExpiry * 60;
+    else if (tokenUnite === 's') return tokenExpiry;
 
     throw new Error("Unexpected token expiry Format " + tokenExpiry);
 }
 const addTokenToBlackList = (token) => {
-    setTimeout(() => {
-        tokenBlackList.delete(token);
-    }, getTokenExpiryInMillisSeconds(process.env.TOKEN_EXPIRY))
-
-    tokenBlackList.add(token);
+    blackListCashe.set(token, true, getTokenExpiryInSeconds(process.env.NODE_ENV || '1d'));
 }
 
 
