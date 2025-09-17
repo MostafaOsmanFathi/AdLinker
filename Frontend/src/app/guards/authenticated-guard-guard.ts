c1c6e4564322b5141674092ffef16bd95368013d
mostafa <mostafa.osman.fathi@gmail.com>
2025-09-17 15:30:35 +0300

    fix inject bug

--- Frontend/src/app/guards/authenticated-guard-guard.ts
+++ Frontend/src/app/guards/authenticated-guard-guard.ts
@@ -2,14 +2,17 @@ import {CanActivateFn} from '@angular/router';
 import {AccountService} from "../services/account-service";
 import {inject} from "@angular/core";
 
-const accountService: AccountService = inject(AccountService);
 
 export const authenticatedGuardGuard: CanActivateFn = async (route, state) => {
+    const accountService: AccountService = inject(AccountService);
     try {
         await accountService.checkAuth().toPromise()
         return true;
     } catch (error) {
-        accountService.logoutUser();
+        try {
+            accountService.logoutUser();
+        } catch (error) {
+        }
         return false;
     }
 };
